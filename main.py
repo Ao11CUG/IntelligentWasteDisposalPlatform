06a56e7b482e9403ffc57c33beef0765892f844d
XiangQingAo <xiangqingao@cug.edu.cn>
2025-06-05 16:57:15 +0800

    1.0 界面初步完成

---
+++ main.py
@@ -0,0 +1,50 @@
+from 界面 import Ui_MainWindow
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import Qt, QPropertyAnimation, QRect, QEasingCurve
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDesktopWidget
+from OpenGL.GL import *
+import  sys
+
+class MyMainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.start_x = None
+        self.start_y = None
+        self.anim=None
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.setWindowFlags(Qt.FramelessWindowHint)  # 设置窗口标志：隐藏窗口边框
+        self.lineEdit.setAttribute(QtCore.Qt.WA_MacShowFocusRect, 0)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            super(MyMainForm, self).mousePressEvent(event)
+            self.start_x = event.x()
+            self.start_y = event.y()
+
+    def mouseReleaseEvent(self, event):
+        self.start_x = None
+        self.start_y = None
+
+    def mouseMoveEvent(self, event):
+        try:
+            super(MyMainForm, self).mouseMoveEvent(event)
+            dis_x = event.x() - self.start_x
+            dis_y = event.y() - self.start_y
+            self.move(self.x() + dis_x, self.y() + dis_y)
+        except:
+            pass
+
+    def effect_shadow_style(self, widget):
+        effect_shadow = QtWidgets.QGraphicsDropShadowEffect(self)
+        effect_shadow.setOffset(12, 12)  # 偏移
+        effect_shadow.setBlurRadius(128)  # 阴影半径
+        effect_shadow.setColor(QColor(155, 230, 237, 150))  # 阴影颜色
+        widget.setGraphicsEffect(effect_shadow)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWin = MyMainForm()
+    myWin.show()
+    sys.exit(app.exec_())
